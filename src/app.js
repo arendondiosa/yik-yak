69d51317e1931a1d4687a5a86a9fb76967f74449
Alejandro E. Rendon <alejorendon@utp.edu.co>
2017-06-30 09:09:58 -0500

    Webpack configuration

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component {
+  render () {
+    return (
+      <div>
+        Hello React!
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+export default App
